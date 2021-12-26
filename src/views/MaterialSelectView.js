ad5df6411fd9354dc7abee3e59fd763cdd0cf2ae
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2021-12-26 15:08:53 +0100

    ADD: Wstępne ustawienie stron dla wspólnego widoku wyboru materiału

--- src/views/MaterialSelectView.js
+++ src/views/MaterialSelectView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, ListGroup, Pagination } from "react-bootstrap";
 import MaterialItemView from "./MaterialItemView";
 import { useGlobalContext } from "../Context";
@@ -9,13 +9,44 @@ import { useGlobalContext } from "../Context";
 
 const MaterialSelectView = () => {
   const { materialsDensityState } = useGlobalContext();
-  const [pageArray, usePageArray] = useState([]);
+  const [actualPage, setActualPage] = useState(0);
+  const [actualMaterials, setActualMaterials] = useState([]);
 
   var tablica = [];
-
   for (let i = 1; i < parseInt(materialsDensityState.length / 5) + 1; i++) {
     tablica.push({ id: i });
   }
+
+  const setupActualMaterials = () => {
+    var newActualMaterials = [];
+
+    let startIndex = actualPage * 5;
+    let endIndex = startIndex + 5;
+    if (endIndex > materialsDensityState.length)
+      endIndex = materialsDensityState.length;
+
+    for (let i = startIndex; i < endIndex; i++) {
+      newActualMaterials.push(materialsDensityState[i]);
+    }
+
+    setActualMaterials(newActualMaterials);
+    console.log(actualMaterials);
+  };
+
+  const nextPage = () => {
+    setActualPage(actualPage + 1);
+    if (actualPage > parseInt(materialsDensityState.length / 5))
+      setActualPage(parseInt(materialsDensityState.length / 5));
+    console.log("Akutalna strona: " + actualPage);
+    setupActualMaterials();
+  };
+
+  const prevPage = () => {
+    setActualPage(actualPage - 1);
+    if (actualPage < 0) setActualPage(0);
+    console.log("Akutalna strona: " + actualPage);
+    setupActualMaterials();
+  };
 
   return (
     <>
@@ -26,7 +57,7 @@ const MaterialSelectView = () => {
         <Row>
           <Col>
             <ListGroup>
-              {materialsDensityState.map((materialItem) => {
+              {actualMaterials.map((materialItem) => {
                 const { id } = materialItem;
                 return <MaterialItemView key={id} {...materialItem} />;
               })}
@@ -37,13 +68,13 @@ const MaterialSelectView = () => {
           <div>
             <Pagination>
               <Pagination.First />
-              <Pagination.Prev />
+              <Pagination.Prev onClick={prevPage} />
               {tablica.map((e) => {
                 return (
                   <Pagination.Item key={e.id}>{parseInt(e.id)}</Pagination.Item>
                 );
               })}
-              <Pagination.Next />
+              <Pagination.Next onClick={nextPage} />
               <Pagination.Last />
             </Pagination>
           </div>
