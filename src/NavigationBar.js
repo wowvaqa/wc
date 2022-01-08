e59e60fe5ecde01666a455e705a9673987c361e7
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2022-01-08 11:02:02 +0100

    Dodanie nav items

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import rollerIcon from "./assetes/rollerIcon.svg";
 
 const NavigationBar = () => {
   return (
@@ -7,11 +8,19 @@ const NavigationBar = () => {
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">WC</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/cuboid">Płaskowniki</Nav.Link>
-            <Nav.Link href="/roller">Pręty</Nav.Link>
-            <Nav.Link href="/materials">Materiały</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+          <Nav variant="pills" className="me-auto" defaultActiveKey="/">
+            <Nav.Item>              
+              <Nav.Link href="/cuboid">Płaskowniki</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/roller">Pręty</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/materials">Materiały</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav.Item>
           </Nav>
         </Container>
       </Navbar>
