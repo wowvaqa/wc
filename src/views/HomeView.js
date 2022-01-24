b658389a7e5dd16279b5fe612af5d972070afa47
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2022-01-24 17:46:34 +0100

    Zmiany na stronie głównej

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Card, Row, Col, Container } from "react-bootstrap";
+import hexImage from "../assetes/hex.svg";
+import rollerImage from "../assetes/roller.svg";
+import tubeImage from "../assetes/tube.svg";
+import cuboidImage from "../assetes/cuboid.svg";
+import squareTubeImage from "../assetes/square_tube.svg";
 
 const HomeView = () => {
   return (
     <>
-      <div className="container">
+      <Container>
         <h2>Kalkulator mas</h2>
         <p>
           Masa teoretyczna liczona jest dla wymiaru nominalnego i nie uwzględnia
@@ -14,7 +20,57 @@ const HomeView = () => {
           <br></br>
           Wybierz kategorię z menu
         </p>
-      </div>
+      </Container>
+
+      <Container fluid="md">
+        <Row xs={1} md={2} className="g-3">
+          <Col>
+            <Card style={{ width: "30rem" }}>
+              <Card.Img variant="top" src={hexImage} />
+              <Card.Body>
+                <Card.Title>Pręty sześciokątne</Card.Title>
+                <Card.Link href="/hex">Masa prętów sześciokątnych</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "30rem" }}>
+              <Card.Img variant="top" src={rollerImage} />
+              <Card.Body>
+                <Card.Title>Pręty okrągłe</Card.Title>
+                <Card.Link href="/roller">Masa prętów okrągłych</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "30rem" }}>
+              <Card.Img variant="top" src={tubeImage} />
+              <Card.Body>
+                <Card.Title>Rury okrągłe</Card.Title>
+                <Card.Link href="/tubes">Masa rur okrągłych</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "30rem" }}>
+              <Card.Img variant="top" src={cuboidImage} />
+              <Card.Body>
+                <Card.Title>Płaskowniki</Card.Title>
+                <Card.Link href="/cuboid">Masa płaskowników</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "30rem" }}>
+              <Card.Img variant="top" src={squareTubeImage} />
+              <Card.Body>
+                <Card.Title>Rury kwadratowe</Card.Title>
+                <Card.Link href="/squaretubes">Masa rur kwadratowych</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
